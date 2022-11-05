9c7af7f9b4bca9d78c15399f271025136a0b1d8d
Abdullah Al Abid <abid35-757@diu.edu.bd>
2022-11-05 18:31:19 +0600

    area converter added to the project
    Half of the code have been written.

--- Extended Calculator/areaConverter.h
+++ Extended Calculator/areaConverter.h
@@ -1,6 +1,56 @@
 #ifndef AREACONVERTER_H_INCLUDED
 #define AREACONVERTER_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "allheaderfile.h"
+void menuArea()
+{
+    printf("\n\t\tArea Calculation Menu\n");
+    printdash();
+    printf("\t\t1. Square meter to others\n\t\t2. Square Centimeter to others\n\t\t3. Square foot to others\n\t\t4. Square kilometer to others\n");
+}
+void meterConverter() {}
+void centimeterConverter() {}
+void footConverter() {}
+void kmConverter() {}
 
+void areaConverter()
+{
+    int choice;
+    while (1)
+    {
+        menuArea();
+        printf("Enter choice: ");
+        switch (choice)
+        {
+        case 1:
+        {
+            meterConverter();
+            break;
+        }
+        case 2:
+        {
+            centimeterConverter();
+            break;
+        }
+        case 3:
+        {
+            footConverter();
+            break;
+        }
+        case 4:
+        {
+            kmConverter();
+            break;
+        }
 
-
+        default:
+        {
+            printf("Invalid choice!!\n");
+            break;
+        }
+        }
+    }
+}
 #endif // AREACONVERTER_H_INCLUDED
